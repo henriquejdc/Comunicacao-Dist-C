6f8fa080bb46982ed70895ddd7da15ba03787557
Henrique José Dalla Corte <riquejdc@gmail.com>
2018-04-16 10:50:29 -0300

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host, port))
 s.setblocking(0)
+arquivo = open('conversa.txt', 'r')
+conteudo = arquivo.readlines()
 
 quitting = False
 print("Server Started.")
@@ -16,6 +18,11 @@ while not quitting:
     try:
         data, addr = s.recvfrom(1024)
         decoded_data = data.decode('utf-8')
+        conteudo.append(decoded_data +'\n')
+        arquivo = open('conversa.txt', 'w')
+        arquivo.writelines(conteudo)
+        arquivo.close()
+
         if ("Quit") in str(decoded_data):
             quitting = True
 
@@ -23,12 +30,12 @@ while not quitting:
             print("Client ", addr, "has connected.")
             clients.append(addr)
 
-        print(time.ctime(time.time()), addr, ": :", decoded_data)
+        print(time.ctime(time.time()), addr, ":", decoded_data)
         time.sleep(0.2)
         for client in clients:
             if addr != client:
                 s.sendto(data, client)
 
     except:
-        pass
+        pass    
 s.close()
